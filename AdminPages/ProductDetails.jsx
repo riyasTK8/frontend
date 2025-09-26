f654453fe9e2774cddd1c3dd44388094f93d1f8f
riyas <mriyastk8@gmail.com>
2025-09-26 11:02:39 +0530

    api instance  corrected

--- AdminPages/ProductDetails.jsx
+++ AdminPages/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "./SideBar";
-import Api from "../../Apiinstance";
+import Api from "../../Apiinstance.jsx";
 
 export default function ProductDetails() {
   const [products, setProducts] = useState([]);
